06d5945a210446de7140d9d88209dc8a236742c2
Mateus da Silva Sousa Dias <41194778+mateuussilvapb@users.noreply.github.com>
2022-06-18 10:49:45 -0300

    Criando formulário de cadastro de usuários

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { UserFormComponent } from './pages/users/user-form/user-form.component';
 import { UserListComponent } from './pages/users/user-list/user-list.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
@@ -7,6 +8,10 @@ const routes: Routes = [
   {
     path: '',
     component: UserListComponent,
+  },
+  {
+    path: 'form',
+    component: UserFormComponent,
   },
 ];
 // =============================================================== //
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { UserListComponent } from './pages/users/user-list/user-list.component';
+import { UserFormComponent } from './pages/users/user-form/user-form.component';
 
 // =============================================================== //
 @NgModule({
   // =============================================================== //
-  declarations: [AppComponent, HeaderComponent, UserListComponent],
+  declarations: [AppComponent, HeaderComponent, UserListComponent, UserFormComponent],
   // =============================================================== //
   imports: [BrowserModule, AppRoutingModule],
   // =============================================================== //
